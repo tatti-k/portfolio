2a0344e9cfb23a6ffe685ad85b77f10bb1c84200
kaito <kaito@takahashikaitonoMacBook-Air.local>
2023-05-04 15:00:15 +0900

    就活用に変更

---
+++ js/aboutJavascript.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var myParagraph = document.getElementById("my-explanation");
+    console.log(myParagraph);
+    if (myParagraph) {
+        var text = myParagraph.innerHTML;
+        text = text.replace("ロボコン同好会", '<a href="https://tutrobo.rm.me.tut.ac.jp/ target="_blank">ロボコン同好会</a>');
+        text = text.replace("AI Tech Lab", '<a href="https://aiiot.jp/ai-tech-lab target="_blank">AI Tech Lab</a>');
+        myParagraph.innerHTML = text;
+    }
+};
+
